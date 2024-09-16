3d6cef7ca57e999d3834659a52e747ad0d761544
Anuj Kumar <anujkumarcoder@gmail.com>
2024-09-16 18:21:21 +0530

    FireBase

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes for different pages
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
